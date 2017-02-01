78ab3c526c21ca1080cb74c6732ce094bc46d4b6
furkansuglun <suglunfurkan@gmail.com>
2017-02-02 01:56:18 +0300

    AndroidTutorial

--- 004.Adding.ScrollView/app/src/main/java/furkan/com/a004addingscrollview/MainActivity.java
+++ 004.Adding.ScrollView/app/src/main/java/furkan/com/a004addingscrollview/MainActivity.java
@@ -2,12 +2,71 @@ package furkan.com.a004addingscrollview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ScrollView scrollView; // --> ViewGroup object
+    private LinearLayout layout;
+    private Button button;
+    private CheckBox checkBox;
+
+    private void init() {
+        scrollView = new ScrollView(this);
+
+        layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        for(int i=1; i <= 20; i++) {
+            if(i % 2 == 0) {
+                checkBox = new CheckBox(this);
+                checkBox.setText("Seçenek " + i);
+
+                //Layout Parameters
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(200,80);
+                params.setMargins(10,5,10,5);
+                params.gravity = Gravity.LEFT;
+                checkBox.setLayoutParams(params);
+
+                layout.addView(checkBox);
+            }
+            else {
+
+                button = new Button(this);
+                button.setText("Buton " + i);
+
+                //Layout Parameters
+                LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(250,90);
+                params2.setMargins(10,5,10,5);
+                params2.gravity = Gravity.RIGHT;
+                button.setLayoutParams(params2);
+
+                layout.addView(button);
+            }
+        }
+
+        //linearlayout --> scrollview
+        scrollView.addView(layout);
+
+        //if we want to horizonal scroll then we have to use HorizontalScrollView!
+        /*
+        scrollView = new HorizontalScrollView(this);
+
+        layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+         */
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        init();
+        setContentView(scrollView);
     }
 }
---
+++ 005.weight.property/app/src/main/java/furkan/com/a005weightproperty/MainActivity.java
@@ -0,0 +1,13 @@
+package furkan.com.a005weightproperty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
